bb642b3100442ac0cef5f66637db58b3bdab0840
Patrick Alcisto <48587+palcisto@users.noreply.github.com>
2024-10-04 16:55:04 -0400

    migrated from Next.js to Astro (#6)
    * migrated from Next.js to Astro
    
    * updated <head> and favicons and touch icons
    
    * updated deps

---
+++ src/styles/homepage.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly "root": string;
+  readonly "logo": string;
+  readonly "longS": string;
+  readonly "longSWrapper": string;
+  readonly "textContainer": string;
+  readonly "message": string;
+};
+export = styles;
+
