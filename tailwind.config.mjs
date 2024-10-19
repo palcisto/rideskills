372c129d743a36c38669f889d7b8a69b1949f7a8
palcisto <patrickalcisto@gmail.com>
2024-10-19 11:34:48 -0400

    completed footer styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#124749',
+        secondary: '#F6B643',
+        tertiary: '#A94029',
+      },
+    },
+  },
+  plugins: [],
+};
