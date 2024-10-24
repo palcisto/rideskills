4629f68fc1e1a77035370a39e86d84d993f16386
palcisto <patrickalcisto@gmail.com>
2024-10-24 14:12:55 -0400

    configure fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,6 +8,11 @@ export default {
         primary: '#124749',
         secondary: '#F6B643',
         tertiary: '#A94029',
+      },
+      fontFamily: {
+        sans: ['Helvetica Neue', ...defaultTheme.fontFamily.sans],
+        display: ['Audiowide', 'system-ui', 'sans-serif'],
+        alternate: ['DM Sans', 'system-ui', 'sans-serif'],
       },
     },
   },
